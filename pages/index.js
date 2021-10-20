f101bad97a6e69297a9c5cfb38979af724c808b1
Denys Strakhov <denys.strakhov@chisw.com>
2021-10-20 17:42:37 +0300

    initial props in ssr

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
-const Landing = () => {
-  return <h1>Landing Page11</h1>;
+import axios from "axios";
+
+const LandingPage = ({currentuser}) => {
+    return <h1>Landing Page11</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({req}) => {
+
+    if (typeof window === 'undefined') {
+        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+            headers: req.headers
+
+        });
+        return data;
+    } else {
+        const {data} = await axios.get('/api/users/currentuser');
+        return data;
+    }
+}
+
+export default LandingPage;
+
